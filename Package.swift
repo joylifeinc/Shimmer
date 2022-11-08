6c4613b2bfce6f8b5f4011ff08b96b00fd763f6d
Victor Bozelli Alvarez <victor.bozelli@withjoy.com>
2022-11-08 04:51:06 -0800

    Implemented support to Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "Shimmer",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Shimmer",
+            targets: ["Shimmer"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Shimmer",
+            path: "FBShimmering"
+        )
+    ]
+)
